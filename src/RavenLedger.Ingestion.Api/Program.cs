9c3c8f36bcd7e8437470698d3e36f7c3fbe44398
BlogDoFT <admin@blogdoft.com.br>
2026-04-19 17:42:40 -0300

    0003 - Logging

--- src/RavenLedger.Ingestion.Api/Program.cs
+++ src/RavenLedger.Ingestion.Api/Program.cs
@@ -1,4 +1,16 @@
+using System.Reflection;
+using Serilog;
+using Serilog.Formatting.Compact;
+
+var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "unknown";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, configuration) =>
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .WriteTo.Console(new CompactJsonFormatter()));
 
 builder.Services.AddOpenApi();
 
@@ -10,5 +22,18 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseSerilogRequestLogging();
 
-app.Run();
+app.Lifetime.ApplicationStarted.Register(() =>
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation("Application started. Version: {Version}, StartedAt: {StartedAt:O}", version, DateTime.UtcNow);
+});
+
+app.Lifetime.ApplicationStopping.Register(() =>
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogWarning("Application shutting down. Version: {Version}, ShutdownAt: {ShutdownAt:O}", version, DateTime.UtcNow);
+});
+
+await app.RunAsync();
